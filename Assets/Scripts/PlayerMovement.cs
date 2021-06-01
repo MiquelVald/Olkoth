cb44e3c7b19823c2e523c08a9390873e3eac6219
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2021-06-01 17:10:18 -0600

    Primeros pasos: añadidos objetos, suelo y cielo

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool jumping = false;
+    public float velH = 2.0f;
+    public float velV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public float Speed = 90.0f;
+    public float rotationS = 1.0f;
+    private Rigidbody Physics;
+    public float jumpForce = 1.0f; 
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        InvokeRepeating("jumpsey", 2.0f, 2.5f);
+        Physics = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, 0.0f, vertical) * Time.deltaTime * Speed);
+
+
+
+
+        yaw += velH * Input.GetAxis("Mouse X");
+        pitch -= velV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * rotationS, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        Physics.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+    }
+}
