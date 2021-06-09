191ab28eeaa7dfaeb08748c805ed35a438522deb
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2021-06-08 22:36:09 -0600

    Añadido scare effect del piano, pinturas y furniture de decoración interior

---
+++ Assets/Scripts/PianoScareTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoScareTrigger : MonoBehaviour
+{
+
+    public AudioSource scareAudioSource;
+    public AudioClip scareSound;
+    private bool hasBeenPlayed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && hasBeenPlayed == false)
+        {
+            scareAudioSource.PlayOneShot(scareSound);
+            hasBeenPlayed = true;
+        }
+    }
+
+
+}
