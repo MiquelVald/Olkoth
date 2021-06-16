6868dc99ddc23b1c786b1a9c10cf6dc8f9c54908
Fred-des <78175361+Fred-des@users.noreply.github.com>
2021-06-15 23:35:24 -0600

    se agrego elementos como dinamica de candil y enemigo

---
+++ Assets/Scripts/Desaparecer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Desaparecer : MonoBehaviour
+{
+
+    public GameObject objeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Enemigo")
+        {
+            objeto.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ObtenerCandil.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObtenerCandil : MonoBehaviour
+{
+
+    public GameObject objeto;
+    public GameObject objeto2;
+    public GameObject objeto3;
+    public GameObject objeto4;
+    public Light Luz1;
+    public Light Luz2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        //Acciones a realizar cuando se detecta una entrada al Trigger.
+        if (c.tag == "Player")
+        {
+            objeto.gameObject.SetActive(true);
+            objeto2.gameObject.SetActive(true);
+            objeto3.gameObject.SetActive(true);
+            objeto4.gameObject.SetActive(false);
+            Luz1.enabled = true;
+            Luz2.enabled = true;
+        }
+    }
+}
