eba075a93aa579bdfb31dde8db620dd362bd74ce
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2021-06-26 01:30:19 -0600

    Eliminado sonido de pasos

--- Assets/Scripts/Selection_Manager.cs
+++ Assets/Scripts/Selection_Manager.cs
@@ -72,7 +72,6 @@ public class Selection_Manager : MonoBehaviour
                 else if (hit.collider.CompareTag("Safe"))
                 {
                     hit.collider.GetComponent<OpenSafe>().ShowSafeCanvas();
-                    hit.collider.GetComponent<stepAudio>().SteppingAudio();
                 }
                 else if (hit.collider.CompareTag("Note"))
                 {
