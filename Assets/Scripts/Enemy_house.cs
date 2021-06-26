313930b5887a2945fc0524d537b4c349e8be4f0d
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2021-06-26 14:56:24 -0600

    Añadidas notas (png) del segundo nivel y primer nivel, falta cargarlas. Añadido menú de controles

---
+++ Assets/Scripts/Enemy_house.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_house : MonoBehaviour
+{
+
+    Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("ScreamerPainting").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
