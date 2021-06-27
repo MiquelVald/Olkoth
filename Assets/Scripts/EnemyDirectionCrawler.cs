1c1cfb193dc1feb82ecca5820cfbc6c852c6b15b
Fred-des <78175361+Fred-des@users.noreply.github.com>
2021-06-26 20:43:20 -0600

    Se agrego enemigo, pantalla de controles y notas en el nivel outside

---
+++ Assets/Scripts/EnemyDirectionCrawler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDirectionCrawler : MonoBehaviour
+{
+    Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
+    private Animator ani;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ani.SetBool("Ataque", true);
+            nav.SetDestination(player.position);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ani.SetBool("Ataque", false);
+            nav.SetDestination(player.position);
+        }
+
+    }
+}
---
+++ Assets/Scripts/EnemyHouse.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHouse : MonoBehaviour
+{
+    public static int countShed = 0;
+    public GameObject objetoenemigo;
+    //public GameObject objetoefecto;
+    public GameObject objetocollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objetoenemigo.gameObject.SetActive(false);
+        //objetoefecto.gameObject.SetActive(false);
+        objetocollider.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        //Acciones a realizar cuando se detecta una entrada al Trigger.
+        if (c.tag == "Player")
+        {
+            countShed++;
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            countShed++;
+
+            if (countShed == 2)
+            {
+                objetoenemigo.gameObject.SetActive(true);
+                //objetoefecto.gameObject.SetActive(true);
+                objetocollider.gameObject.SetActive(false);
+
+            }
+        }
+    }
+}
